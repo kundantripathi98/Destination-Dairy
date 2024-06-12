0326713fd873b02e98ebd466a48b8cf0658e68e9
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-06-12 09:49:45 +0530

    components added

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,5 @@
+const Logo = () => {
+    return <h1>Destination Diary</h1>
+}
+
+export default Logo;
---
+++ src/Components/Stats.jsx
@@ -0,0 +1,5 @@
+const Stats = () => {
+    return <h1>Destination Diary</h1>
+}
+
+export default Stats;
