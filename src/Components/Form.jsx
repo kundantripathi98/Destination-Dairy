388aa8b9340cce3a83e66a4b1d7cf4ac8d0be742
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-06-12 16:42:27 +0530

    updated

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
+import {initialItems} from '../Components/PackingList';
+
 const Form = () => {
-    return <form className="add-form">
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+
+    const handleForm = (e)=>{
+        e.preventDefault();
+        const newItem = {
+            id: Date.now(),
+            description,
+            quantity,
+            package: false
+        };
+        console.log(newItem);
+
+        initialItems.push(newItem);
+        setDescription("");
+        setQuantity(1);
+    }
+
+    return <form className="add-form" onSubmit={handleForm}>
         <h3>What do you need for your trip?</h3>
 
-        <select name="" id="">
+        <select name="" id="" value={quantity} onChange={(e)=>setQuantity(Number(e.target.value))}>
             {Array.from({length: 20}).map((item, index)=>(
                 <option value={index + 1} key={index}>{index + 1}</option>
             ))}
         </select>
 
-        <input type="text" placeholder="Item..."/>
+        <input type="text" placeholder="Item..." value={description} onChange={(e)=>setDescription(e.target.value)}/>
 
         <button>Add</button>
     </form>
--- src/Components/PackingList.jsx
+++ src/Components/PackingList.jsx
@@ -1,9 +1,9 @@
 import Item from "./Item";
 
-const initialItems = [
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
-    { id: 3, description: "Charger", quantity: 1, packed: true },
+export const initialItems = [
+    // { id: 1, description: "Passports", quantity: 2, packed: false },
+    // { id: 2, description: "Socks", quantity: 12, packed: false },
+    // { id: 3, description: "Charger", quantity: 1, packed: true },
   ];
 
 const PackingList = () => {
