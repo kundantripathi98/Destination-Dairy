732c3e42d4f1f310a43c54c93936704630542841
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-07-13 22:54:18 +0530

    updated

--- src/Components/PackingList.jsx
+++ src/Components/PackingList.jsx
@@ -2,7 +2,6 @@ import Item from "./Item";
 
 export const initialItems = [
     // { id: 1, description: "Passports", quantity: 2, packed: false },
-    // { id: 2, description: "Socks", quantity: 12, packed: false },
 
   ];
 
