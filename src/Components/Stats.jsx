744e23188fb429a06f85b1fd82ff95d746fca9cd
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-08-01 18:19:05 +0530

    updated

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -2,11 +2,20 @@ const Stats = ({items}) => {
     const numItems = items.length;
     const packedNum = items.filter((item)=>item.packed).length;
     const percent = Math.floor((packedNum / numItems) * 100);
-    return <footer className="stats">
+    if(percent !== 100){
+        return <footer className="stats">
         <em>
            💼 You have  {numItems === 0 ? "0": `${numItems}`} {numItems <= 1 ? "item" : "items"} on your list, and you already packed {packedNum} ({isNaN(percent) ? "0" : percent}%)
         </em>
     </footer>
+    }
+    else{
+        return <footer className="stats">
+        <em>
+           You got everything! Ready to go✈️ 
+        </em>
+    </footer>
+    }
 }
 
 export default Stats;
